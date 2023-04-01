2e90fa843137aa4418ff4217dd2c1d7716664eef
196code-gray <doco827@gmail.com>
2023-04-01 23:59:58 +0900

    20일 스터디 멤버만

---
+++ src/main/java/com/samkim/member/Example.java
@@ -0,0 +1,11 @@
+package com.samkim.member;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/appointment")
+public class Example {
+
+}
